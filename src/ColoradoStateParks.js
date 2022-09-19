6b5c5bb651c17098a7be2752f0d75d93e0284ad1
thadeus <thadeusnyariki94@gmail.com>
2022-09-19 15:46:54 +0300

    Done assignmnent

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,24 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import howManyParks from "./parks/howManyParks"
+import MesaVerde from "./parks/MesaVerde";
+// import {trees, wildlife, elevation} from "./parks/RockyMountain"
+import * as RMFunctions from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  // console.log(trees)
+  // wildlife()
+  // elevation()
+  console.log(RMFunctions.trees);
+  RMFunctions.wildlife();
+  RMFunctions.elevation();
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <h1>Colorado State Parks</h1>
+      <MesaVerde />
+    </div>
+  )
 }
+
+export default ColoradoStateParks
